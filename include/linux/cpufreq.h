382e1f20ce6e3fa9ab65588479abc292d17a7e67
engstk <eng.stk@sapo.pt>
2018-10-10 18:49:22 +0100

    CPUFreq: Port Blu_Schedutil Governor from OP6 (Initial Bringup)
    Import from linux 4.9 android-common plus additional changes and fixes
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Port and Code Adaptation for hi3660
    
    Ported by: JBolho <hugo.ss.android@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -687,6 +687,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL)
+extern struct cpufreq_governor blu_schedutil_gov;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&blu_schedutil_gov)
 #endif
 
 /*********************************************************************
