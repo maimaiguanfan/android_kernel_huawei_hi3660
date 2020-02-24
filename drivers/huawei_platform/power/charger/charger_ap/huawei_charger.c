10527850298f7b11edfabe8fa6fbc51adf586e6d
麥麥觀飯 <979436500@qq.com>
2020-02-24 19:32:32 +0800

    Implement USB Fast Charging

--- drivers/huawei_platform/power/charger/charger_ap/huawei_charger.c
+++ drivers/huawei_platform/power/charger/charger_ap/huawei_charger.c
@@ -78,6 +78,17 @@
 
 /* battery safety notifier head */
 BLOCKING_NOTIFIER_HEAD(charger_event_notify_head);
+
+#ifdef CONFIG_OTG_GPIO_ID
+#include <huawei_platform/usb/huawei_ycable.h>
+
+extern struct ycable_info *ycable;
+#endif
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 /*adaptor test result*/
 struct adaptor_test_attr adptor_test_tbl[] = {
 	{TYPE_SCP, "SCP", DETECT_FAIL},
@@ -2316,6 +2327,13 @@ static void charge_select_charging_current(struct charge_device_info *di)
 	case CHARGER_TYPE_USB:
 		di->input_current = di->core_data->iin_usb;
 		di->charge_current = di->core_data->ichg_usb;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge > 0)
+		{
+			di->input_current = CHARGE_CURRENT_0800_MA;
+			di->charge_current = CHARGE_CURRENT_0800_MA;
+		}
+#endif
 		break;
 	case CHARGER_TYPE_NON_STANDARD:
 		di->input_current = di->core_data->iin_nonstd;
