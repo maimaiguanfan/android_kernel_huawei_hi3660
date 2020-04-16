00163e4068658a46aeeb5a78a7bc223cdbbda6b8
麥麥觀飯 <979436500@qq.com>
2020-04-16 09:57:42 +0800

    net: IP defrag: encapsulate rbtree defrag code into callable functions

--- net/ipv4/inet_fragment.c
+++ net/ipv4/inet_fragment.c
@@ -315,6 +315,28 @@ void inet_frag_destroy(struct inet_frag_queue *q, struct inet_frags *f)
 }
 EXPORT_SYMBOL(inet_frag_destroy);
 
+unsigned int inet_frag_rbtree_purge(struct rb_root *root)
+{
+	struct rb_node *p = rb_first(root);
+	unsigned int sum = 0;
+
+	while (p) {
+		struct sk_buff *skb = rb_entry(p, struct sk_buff, rbnode);
+
+		p = rb_next(p);
+		rb_erase(&skb->rbnode, root);
+		while (skb) {
+			struct sk_buff *next = FRAG_CB(skb)->next_frag;
+
+			sum += skb->truesize;
+			kfree_skb(skb);
+			skb = next;
+		}
+	}
+	return sum;
+}
+EXPORT_SYMBOL(inet_frag_rbtree_purge);
+
 static struct inet_frag_queue *inet_frag_intern(struct netns_frags *nf,
 						struct inet_frag_queue *qp_in,
 						struct inet_frags *f,
