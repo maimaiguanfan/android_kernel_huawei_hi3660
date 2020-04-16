5263c9c9ea9b3ab2c85b3cdf99b362306d7f5938
麥麥觀飯 <979436500@qq.com>
2020-04-16 08:24:14 +0800

    tcp: tcp_grow_window() needs to respect tcp_space()

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -451,6 +451,9 @@ static int __tcp_grow_window(const struct sock *sk, const struct sk_buff *skb)
 static void tcp_grow_window(struct sock *sk, const struct sk_buff *skb)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
+	int room;
+
+	room = min_t(int, tp->window_clamp, tcp_space(sk)) - tp->rcv_ssthresh;
 #ifdef CONFIG_MPTCP
 	struct sock *meta_sk = mptcp(tp) ? mptcp_meta_sk(sk) : sk;
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
@@ -481,9 +484,7 @@ static void tcp_grow_window(struct sock *sk, const struct sk_buff *skb)
 	}
 #else
 	/* Check #1 */
-	if (tp->rcv_ssthresh < tp->window_clamp &&
-	    (int)tp->rcv_ssthresh < tcp_space(sk) &&
-	    !tcp_under_memory_pressure(sk)) {
+	if (room > 0 && !tcp_under_memory_pressure(sk)) {
 		int incr;
 
 		/* Check #2. Increase window, if skb with such overhead
