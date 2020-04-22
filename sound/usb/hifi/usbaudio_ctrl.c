fce956286c20c3dbd9a5ee9f17ad92238210a2bb
麥麥觀飯 <979436500@qq.com>
2020-04-22 20:57:08 +0800

    Fix some Werrors again

--- sound/usb/hifi/usbaudio_ctrl.c
+++ sound/usb/hifi/usbaudio_ctrl.c
@@ -46,12 +46,12 @@
 #include "clock.h"
 #include "usbaudio_dsp_client.h"
 #include "usbaudio_ioctl.h"
-#include "huawei_platform/log/imonitor.h"
-#include "huawei_platform/log/imonitor_keys.h"
+#include <huawei_platform/log/imonitor.h>
+#include <huawei_platform/log/imonitor_keys.h>
 
 
 #ifdef CLT_AUDIO
-#include "usbaudio_test.h"
+#include "usbaudio.h"
 #endif
 
 #ifdef CONFIG_HUAWEI_DSM
