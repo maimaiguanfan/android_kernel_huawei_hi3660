aa50da90485f693fc1484f1543833c2cf49e7e2a
麥麥觀飯 <979436500@qq.com>
2020-04-23 13:56:30 +0800

    Edit pack scripts

--- pack.sh
+++ pack.sh
@@ -2,8 +2,8 @@
 echo " "
 echo "***Packing Images***"
 echo " "
-tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-03-05  --output Pangu_kernel.img
-tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-03-05  --output Pangu_kernel-permissive.img
+tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-03-05  --output PK_EROFS.img
+tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-03-05  --output PK_EROFS_PM.img
 echo "***Done!***"
 echo " "
 rm -rf tools/AnyKernel3/*.zip
@@ -15,10 +15,10 @@ git pull upstream master
 echo " "
 echo "***Packing Zip***"
 echo " "
-zip -r9 Pangu_kernel.zip * ".git" > /dev/null
+zip -r9 PK_EROFS.zip * ".git" > /dev/null
 cd ../..
-cp tools/AnyKernel3/Pangu_kernel.zip Pangu_kernel.zip
-rm -rf tools/AnyKernel3/Pangu_kernel.zip
+cp tools/AnyKernel3/PK_EROFS.zip PK_EROFS.zip
+rm -rf tools/AnyKernel3/PK_EROFS.zip
 rm -rf tools/AnyKernel3/Image.gz
 echo "***Done!***"
 echo " "
