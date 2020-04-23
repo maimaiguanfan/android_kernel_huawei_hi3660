d39a65b88d1451fe8ed5d6b390527df1f71263fd
麥麥觀飯 <979436500@qq.com>
2020-04-23 08:37:06 +0800

    Add scripts for M5

---
+++ build_M5.sh
@@ -0,0 +1,14 @@
+
+
+export PATH=$PATH:/home/leo-mak/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin
+export CROSS_COMPILE=/home/leo-mak/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+
+export GCC_COLORS=auto
+export ARCH=arm64
+
+mkdir out
+
+make ARCH=arm64 O=out Pangu_M5_defconfig  > /dev/null
+
+
+make ARCH=arm64 O=out -j64 > /dev/null
